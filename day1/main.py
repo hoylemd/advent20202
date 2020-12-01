7a0deff550252623bc0c2018c37abed4a85eef6b
Michael D. Hoyle <hoylemd@gmail.com>
2020-12-01 10:44:42 -0500

    fix bugs & part 1 for real

--- day1/main.py
+++ day1/main.py
@@ -6,12 +6,11 @@ expenses = sorted(int(item)for item in items)
 left = 0
 right = -1
 
-while total := expenses[left] + expenses[right] != 2020:
+while (total := expenses[left] + expenses[right]) != 2020:
     if total < 2020:
         left += 1
 
     if total > 2020:
-        left -= 1
-
+        right -= 1
 
 print(expenses[left] * expenses[right])
