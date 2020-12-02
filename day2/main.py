3572200150aa7a4a53e5d7a3a31277c8550eb830
Michael D. Hoyle <hoylemd@gmail.com>
2020-12-02 11:08:27 -0500

    first attempt at part 1

--- day2/main.py
+++ day2/main.py
@@ -6,11 +6,35 @@ with open(FILENAME) as fp:
 
 
 def parse_entry(entry):
-    pass
+    parts = entry[:-1].split(': ')
+    password = parts[1]
+
+    policy_parts = parts[0].split(' ')
+    range_parts = policy_parts[0].split('-')
+
+    policy = {
+        'char': policy_parts[1],
+        'min': int(range_parts[0]),
+        'max': int(range_parts[1]),
+        'raw': parts[0]
+    }
+
+    return policy, password
 
 
 def is_valid(entry):
     policy, password = parse_entry(entry)
+
+    n_c = password.count(policy['char'])
+
+    print(f"char '{policy['char']}' appears {n_c} times in pw {password} ({policy['raw']})")
+    if n_c < policy['min']:
+        return False
+
+    if n_c > policy['max']:
+        return False
+
+    return True
 
 
 answer = 0
