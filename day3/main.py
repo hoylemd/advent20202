f8a027d101a9b405ef991b9e04d4a8819bed1016
Michael D. Hoyle <hoylemd@gmail.com>
2020-12-03 14:11:13 -0500

    part 1

--- day3/main.py
+++ day3/main.py
@@ -2,13 +2,30 @@
 FILENAME = 'test.txt'
 
 with open(FILENAME) as fp:
-    items = fp.readlines()
+    rows = [line.strip() for line in fp.readlines()]
 
-answer = 0
+trees = 0
+x, y = 0, 0
+d_x, d_y = 3, 1
 
-for item in items:
-    # --- do work here ---
-    pass
+dest_y = len(rows) - 1
+n_cols = len(rows[0])
+n_wraps = 0
+symbol = rows[y][x]
+
+while y < dest_y:
+    print(f"{rows[y] * n_wraps}{rows[y][:x]}{symbol}{rows[y][x+1:]}")
+    new_x = (x + d_x) % n_cols
+    if new_x < x:
+        n_wraps += 1
+
+    x = new_x
+    y += d_y
+
+    symbol = 'O'
+    if rows[y][x] == '#':
+        trees += 1
+        symbol = 'X'
 
 
-print(answer)
+print(trees)
