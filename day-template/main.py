66a743bb0f70be460985e6a1b57848b09cccaf8a
Michael D. Hoyle <hoylemd@gmail.com>
2020-12-04 11:43:35 -0500

    add debug stuff to template

--- day-template/main.py
+++ day-template/main.py
@@ -1,12 +1,16 @@
 # FILENAME = 'data.txt'
 FILENAME = 'test.txt'
 
+DEBUG = True
+
 with open(FILENAME) as fp:
     items = fp.readlines()
 
 answer = 0
 
 for item in items:
+    if DEBUG:
+        print(f"{item=}")
     # --- do work here ---
     pass
 
