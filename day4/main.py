913aa1353bc453283c5e5b97268ccf9a28b6b064
Michael D. Hoyle <hoylemd@gmail.com>
2020-12-04 12:10:18 -0500

    implement field validation (not solved!)

--- day4/main.py
+++ day4/main.py
@@ -1,7 +1,10 @@
-# FILENAME = 'data.txt'
-FILENAME = 'test.txt'
+import re
 
 DEBUG = False
+
+FILENAME = 'data.txt'
+if DEBUG:
+    FILENAME = 'test.txt'
 
 with open(FILENAME) as fp:
     lines = fp.readlines()
@@ -21,6 +24,63 @@ def gen_passports(lines):
             k, v = field.split(':')
             pport[k] = v
 
+    yield pport
+
+
+def is_between(value, at_least, at_most):
+    parsed = int(value)
+    if parsed < at_least:
+        return False
+
+    if parsed > at_most:
+        return False
+
+    return True
+
+
+def year_validator(at_least, at_most):
+    def valid8r(value):
+        if re.match(r'[0-9]{4}', value):
+            return is_between(value, at_least, at_most)
+
+        return False
+
+    return valid8r
+
+
+def height_validator(value):
+    magnitude = value[:-2]
+    unit = value[-2:]
+
+    if unit == 'cm':
+        return is_between(magnitude, 150, 193)
+    if unit == 'in':
+        return is_between(magnitude, 59, 76)
+
+    return False
+
+
+def hair_validator(value):
+    return re.match(r'#[0-9a-f]{6}', value)
+
+
+def eye_validator(value):
+    return value in ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']
+
+
+def pid_validator(value):
+    return re.match(r'[0-9]{9}', value)
+
+
+VALID8RS = {
+    'byr': year_validator(1920, 2002),
+    'iyr': year_validator(2010, 2020),
+    'eyr': year_validator(2020, 2030),
+    'hgt': height_validator,
+    'hcl': hair_validator,
+    'ecl': eye_validator,
+    'pid': pid_validator,
+}
 
 REQUIRED_FIELDS = set(['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid'])
 OPTIONAL_FIELDS = ['cid']
@@ -32,7 +92,17 @@ def validate_passport(passport):
     satisfied = REQUIRED_FIELDS.intersection(present_keys)
     if DEBUG:
         print(f"{present_keys=}{satisfied=}")
-    return satisfied == REQUIRED_FIELDS
+
+    if satisfied != REQUIRED_FIELDS:
+        return False
+
+    for key, value in passport.items():
+        validator = VALID8RS.get(key)
+        if validator is None or validator(value):
+            continue
+        return False
+
+    return True
 
 
 answer = 0
@@ -40,5 +110,8 @@ answer = 0
 for pport in gen_passports(lines):
     if validate_passport(pport):
         answer += 1
+    else:
+        if DEBUG:
+            print(f"invalid: {pport}")
 
 print(answer)
