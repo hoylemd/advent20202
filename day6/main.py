3cccae6be2ea6af4d7bf75e1006728b80bb12f5e
Michael D. Hoyle <hoylemd@gmail.com>
2020-12-06 16:17:24 -0500

    part 1

--- day6/main.py
+++ day6/main.py
@@ -4,15 +4,33 @@ FILENAME = 'test.txt'
 # FILENAME = 'data.txt'
 
 with open(FILENAME) as fp:
-    items = [line.strip() for line in fp.readlines()]
+    lines = [line.strip() for line in fp.readlines()]
 
 answer = 0
 
-for item in items:
+affirmatives = set()
+
+for line in lines:
     if DEBUG:
-        print(f"{item=}")
-    # --- do work here ---
-    pass
+        print(f"{line=}")
+
+    if not line:
+        if DEBUG:
+            print(f"Group complete: {affirmatives=}({len(affirmatives)})")
+        answer += len(affirmatives)
+        if DEBUG:
+            print(f"{answer=}")
+        affirmatives = set()
+        continue
+
+    affirmatives |= set(line)
+
+
+if DEBUG:
+    print(f"Group complete: {affirmatives=}({len(affirmatives)})")
+answer += len(affirmatives)
+if DEBUG:
+    print(f"{answer=}")
 
 if DEBUG:
     print('---- DEBUG ENDS ----')
