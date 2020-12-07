452eebca126fff61c4bc56a4a2f0697938682aae
Michael D. Hoyle <hoylemd@gmail.com>
2020-12-07 16:20:56 -0500

    implement rule parsing

--- day7/main.py
+++ day7/main.py
@@ -3,16 +3,49 @@ DEBUG = True
 FILENAME = 'test.txt'
 # FILENAME = 'data.txt'
 
+
+def parse_content_spec(spec):
+    deets = spec.split(' bag')[0]
+
+    parts = deets.split(' ', 1)
+    colour = parts[1]
+    number = int(parts[0])
+
+    return colour, number
+
+
+class Rule:
+    def __init__(self, src):
+        self.src = src
+
+        self.contents = {}
+
+        # [:-1] strips the trailing .
+        parts = src[:-1].split(' bags contain ')
+        self.colour = parts[0]
+
+        if parts[1] == 'no other bags':
+            return
+
+        for spec in parts[1].split(', '):
+            c, n = parse_content_spec(spec)
+            self.contents[c] = n
+
+    def __str__(self):
+        return f"{self.colour}: {self.contents}"
+
+
 with open(FILENAME) as fp:
-    items = [line.strip() for line in fp.readlines()]
+    rules = [line.strip() for line in fp.readlines()]
 
 answer = 0
 
-for item in items:
+for spec in rules:
     if DEBUG:
-        print(f"{item=}")
-    # --- do work here ---
-    pass
+        print(f"{spec=}")
+
+    rule = Rule(spec)
+    print(rule)
 
 if DEBUG:
     print('---- DEBUG ENDS ----')
