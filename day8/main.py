b3e4eb55e121515b2173383b8037a1fbbb5e51c1
Michael D. Hoyle <hoylemd@gmail.com>
2020-12-08 09:58:12 -0500

    implement part 1

--- day8/main.py
+++ day8/main.py
@@ -3,18 +3,44 @@ DEBUG = True
 FILENAME = 'test.txt'
 # FILENAME = 'data.txt'
 
+
+def parse_instruction(line):
+    op, arg = line.strip().split(' ', 1)
+    return op, int(arg)
+
+
 with open(FILENAME) as fp:
-    items = [line.strip() for line in fp.readlines()]
+    instructions = [parse_instruction(line) for line in fp.readlines()]
 
-answer = 0
+acc = 0
+instr_idx = 0
+crumbs = set()
 
-for item in items:
+while True:
+    try:
+        instr = instructions[instr_idx]
+    except IndexError:
+        break
+
     if DEBUG:
-        print(f"{item=}")
-    # --- do work here ---
-    pass
+        print(f"{instr=}")
+
+    if instr_idx in crumbs:
+        break
+
+    crumbs.add(instr_idx)
+
+    op, arg = instr
+
+    if op == 'acc':
+        acc += arg
+
+    if op == 'jmp':
+        instr_idx += arg
+    else:
+        instr_idx += 1
 
 if DEBUG:
     print('---- DEBUG ENDS ----')
 
-print(answer)
+print(acc)
