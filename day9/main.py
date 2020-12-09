1d7f294ac977b304e44381586dfb4f8a68fe75a7
Michael D. Hoyle <hoylemd@gmail.com>
2020-12-09 14:52:30 -0500

    first draft of part 1

--- day9/main.py
+++ day9/main.py
@@ -1,20 +1,53 @@
 DEBUG = True
 
+
+def debug(*args):
+    if DEBUG:
+        print(*args)
+
+
+preamble_length = 5
 FILENAME = 'test.txt'
+# preamble_length = 25
 # FILENAME = 'data.txt'
 
+
 with open(FILENAME) as fp:
-    items = [line.strip() for line in fp.readlines()]
+    items = [int(line) for line in fp.readlines()]
+
+
+def validate_num(num, history):
+    debug(f"{num=} {history=}")
+
+    hist = sorted(history)
+    left, right = 0, len(history) - 1
+
+    while(left != right and (cand := hist[left] + hist[right]) != num):
+        if cand < num:
+            left += 1
+        else:
+            right -= 1
+
+    if left == right:
+        return False
+
+    debug(f"{num} = {hist[left]} + {hist[right]}")
+    return True
+
 
 answer = 0
+i = preamble_length
+while i < len(items):
+    item = items[i]
+    debug(f"{item=}")
 
-for item in items:
-    if DEBUG:
-        print(f"{item=}")
-    # --- do work here ---
-    pass
+    history = items[i - preamble_length:i]
+    if not validate_num(item, history):
+        answer = item
+        break
 
-if DEBUG:
-    print('---- DEBUG ENDS ----')
+    i += 1
+
+debug('---- DEBUG ENDS ----')
 
 print(answer)
