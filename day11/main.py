4991e7c94c88066f99bc6eb38c4030bb7bc4ab14
Michael D. Hoyle <hoylemd@gmail.com>
2020-12-12 18:08:38 -0500

    part 2

--- day11/main.py
+++ day11/main.py
@@ -24,7 +24,8 @@ adj_adjs = [
 ]
 
 
-def update_cell(lobby, x, y):
+def update_cell_part_1(lobby, x, y):
+    """Part 1"""
     state = lobby[y][x]
     if state == '.':
         return state, False
@@ -54,6 +55,46 @@ def update_cell(lobby, x, y):
     return state, False
 
 
+def occupied_in_dir(lobby, x, y, d_x, d_y):
+    while True:
+        x += d_x
+        y += d_y
+
+        if x < 0 or y < 0:
+            return False
+
+        try:
+            t_v = lobby[y][x]
+        except IndexError:
+            return False
+
+        if t_v == '#':
+            return True
+
+        if t_v == 'L':
+            return False
+
+
+def update_cell_part_2(lobby, x, y):
+    """Part 2"""
+    state = lobby[y][x]
+    if state == '.':
+        return state, False
+
+    occupied = 0
+    for dx, dy in adj_adjs:
+        if occupied_in_dir(lobby, x, y, dx, dy):
+            occupied += 1
+
+    if state == 'L' and occupied == 0:
+        return '#', True
+
+    if state == '#' and occupied > 4:
+        return 'L', True
+
+    return state, False
+
+
 def update_lobby(lobby):
     changed = False
     new_lobby = []
@@ -62,7 +103,7 @@ def update_lobby(lobby):
         x = 0
         new_row = []
         for cell in row:
-            new_cell, cell_changed = update_cell(lobby, x, y)
+            new_cell, cell_changed = update_cell_part_2(lobby, x, y)
             new_row.append(new_cell)
             changed |= cell_changed
             x += 1
