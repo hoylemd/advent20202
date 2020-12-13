4430b39eec1811fe4df5861f3ad30a956cbec7f8
Michael D. Hoyle <hoylemd@gmail.com>
2020-12-13 13:04:33 -0500

    start part 2

--- day13/main.py
+++ day13/main.py
@@ -10,9 +10,6 @@ def debug(*args):
 
 def find_wait_and_bus(epoch, schedule):
     """Part 1"""
-    debug(f"{epoch=}")
-    debug(f"{schedule=}")
-
     earliest = None
     bus_no = None
     for bus in schedule:
@@ -33,19 +30,34 @@ def find_wait_and_bus(epoch, schedule):
     return wait, bus_no
 
 
+def indexed(iterator):
+    i = 0
+    for item in iterator:
+        yield item, i
+        i += 1
+
+
 def validate_winning_timestamp(timestamp, schedule):
     """Part 2"""
-    pass
+    offsets = {int(bus): i for bus, i in indexed(schedule) if bus != 'x'}
+    earliest = 0
+
+    debug(offsets)
+
+    return earliest
 
 
 lines = [line.strip() for line in fileinput.input()]
 
 epoch = int(lines[0])
 schedule = [b for b in lines[1].split(',')]
+debug(f"{epoch=}")
+debug(f"{schedule=}")
 
-wait, bus = find_wait_and_bus(epoch, schedule)
+# Part 1
+# wait, bus = find_wait_and_bus(epoch, schedule)
 
-answer = wait * bus
+answer = validate_winning_timestamp(0, schedule)
 
 debug('---- DEBUG ENDS ----')
 
