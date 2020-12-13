9510392322d57f26dd1bbef359c47be244579ce8
Michael D. Hoyle <hoylemd@gmail.com>
2020-12-12 20:51:57 -0500

    and part 2

--- day12/main.py
+++ day12/main.py
@@ -36,6 +36,7 @@ def turn(direction, distance, heading):
 
 
 def execute(course):
+    """Part 1"""
     x, y = 0, 0
     heading = 'E'
 
@@ -61,6 +62,46 @@ def execute(course):
     return x, y
 
 
+def rotate_right(x, y):
+    return -y, x
+
+
+def rotate_waypoint(direction, distance, x, y):
+    steps = int(distance / 90)
+
+    if direction == 'L':
+        steps *= -1
+
+    steps = steps % 4
+
+    for i in range(steps):
+        x, y = rotate_right(x, y)
+
+    return x, y
+
+
+def execute_waypoints(course):
+    """Part 2"""
+    x, y = 0, 0
+    wx, wy = 10, -1
+
+    for item in course:
+        instr, arg = item[0], int(item[1:])
+
+        if instr == 'F':
+            for i in range(arg):
+                x += wx
+                y += wy
+
+        if instr in heading_components:
+            wx, wy = move_direction(instr, arg, wx, wy)
+
+        if instr in ['R', 'L']:
+            wx, wy = rotate_waypoint(instr, arg, wx, wy)
+
+    return x, y
+
+
 def manhattan_distance(x, y):
     debug(f"mann: {x} {y}")
     if x < 0:
@@ -74,7 +115,7 @@ def manhattan_distance(x, y):
 
 course = [line.strip() for line in fileinput.input()]
 
-x, y = execute(course)
+x, y = execute_waypoints(course)
 answer = manhattan_distance(x, y)
 
 debug('---- DEBUG ENDS ----')
