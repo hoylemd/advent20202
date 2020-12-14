1c0a13445dbc1cab63399a65569d382228375254
Michael D. Hoyle <hoylemd@gmail.com>
2020-12-14 16:03:18 -0500

    part 1

--- day14/main.py
+++ day14/main.py
@@ -1,16 +1,11 @@
 import fileinput
 
-DEBUG = True
+DEBUG = False
 
 
 def debug(*args):
     if DEBUG:
         print(*args)
-
-
-lines = [line.strip() for line in fileinput.input()]
-
-mask = lines.pop(0).split(' ')[-1]
 
 
 def parse_instruction(line):
@@ -21,19 +16,48 @@ def parse_instruction(line):
     return location, val
 
 
+def print_bin_num(num, width):
+    binny = f"{num:b}"
+    padding = width - len(binny)
+
+    return f"{'0' * padding}{binny} = {num}"
+
+
 def apply_mask(mask, value):
+    debug(f"applying mask to {value}")
+    width = len(mask)
+    t_mask = int(mask.replace('X', '0'), 2)
+    f_mask = int(mask.replace('X', '1'), 2)
+
+    debug(mask)
+    debug(print_bin_num(value, width))
+    debug(print_bin_num(t_mask, width))
+    debug(print_bin_num(f_mask, width))
+
+    value = value | t_mask
+    value = value & f_mask
+
+    debug(print_bin_num(value, width))
+
     return value
 
+
+lines = [line.strip() for line in fileinput.input()]
 
 answer = 0
 memory = {}
 
-debug(f"{mask=}")
 for line in lines:
-    l, v = parse_instruction(line)
-    debug(f"Set memory location {l} to {v}")
+    if line.startswith('mask'):
+        mask = line.split(' ')[-1]
+        continue
 
-    memory[l] = apply_mask(mask, v)
+    l, v = parse_instruction(line)
+
+    masked = apply_mask(mask, v)
+    debug(f"Set memory location {l} to {masked}")
+
+    memory[l] = masked
 
 answer = sum(memory.values())
 
