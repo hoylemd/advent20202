f14d620c611e0c904105c2f4ad607bf652749593
Michael D. Hoyle <hoylemd@gmail.com>
2020-12-19 22:38:54 -0500

    part 2

--- day14/main.py
+++ day14/main.py
@@ -9,6 +9,7 @@ def debug(*args):
 
 
 def parse_instruction(line):
+    """Parse the instruction line into target address and the value"""
     parts = line.split('] = ', 1)
     val = int(parts[-1])
     location = int(parts[0][4:])
@@ -16,14 +17,19 @@ def parse_instruction(line):
     return location, val
 
 
-def print_bin_num(num, width):
+def pad_bin_num(num, width):
     binny = f"{num:b}"
     padding = width - len(binny)
 
-    return f"{'0' * padding}{binny} = {num}"
+    return f"{'0' * padding}{binny}"
+
+
+def print_bin_num(num, width):
+    return f"{pad_bin_num(num, width)} = {num}"
 
 
 def apply_mask(mask, value):
+    """Part 1"""
     debug(f"applying mask to {value}")
     width = len(mask)
     t_mask = int(mask.replace('X', '0'), 2)
@@ -42,6 +48,42 @@ def apply_mask(mask, value):
     return value
 
 
+class Mask:
+    def __init__(self, src):
+        self.src = src
+        self.width = len(src)
+
+    def decode_address(self, addr):
+        """Decode a source address into a list of addresses.
+
+        :param int addr: The source address in integer form.
+        :returns [int]: The list of masked addresses in integer form.
+        """
+        addr_strs = [pad_bin_num(addr, self.width)]
+        for i, bit in enumerate(self.src):
+            new_strs = []
+            if bit == '0':
+                pass
+            elif bit == '1':
+                new_strs = [
+                    f"{addr_str[:i]}1{addr_str[i+1:]}"
+                    for addr_str in addr_strs
+                ]
+            elif bit == 'X':
+                for addr_str in addr_strs:
+                    new_strs += [
+                        f"{addr_str[:i]}1{addr_str[i+1:]}",
+                        f"{addr_str[:i]}0{addr_str[i+1:]}"
+                    ]
+            else:
+                raise ValueError(f"{bit=} wat")
+
+            if new_strs:
+                addr_strs = new_strs
+
+        return [int(addr_str, 2) for addr_str in addr_strs]
+
+
 lines = [line.strip() for line in fileinput.input()]
 
 answer = 0
@@ -49,15 +91,17 @@ memory = {}
 
 for line in lines:
     if line.startswith('mask'):
-        mask = line.split(' ')[-1]
+        mask = Mask(line.split(' ')[-1])
         continue
 
-    l, v = parse_instruction(line)
+    loc, v = parse_instruction(line)
 
-    masked = apply_mask(mask, v)
-    debug(f"Set memory location {l} to {masked}")
+    addrs = mask.decode_address(int(loc))
 
-    memory[l] = masked
+    for addr in addrs:
+        memory[addr] = v
+        # masked = apply_mask(mask, v)
+        debug(f"apply to location {addr}: {v}")
 
 answer = sum(memory.values())
 
